6523ebe6e8b54a22bef0b18eb5d5bc96d8c31db7
hasanhayat <aylskhsmvz.v@gmail.com>
2024-11-25 22:09:52 +0500

    first problem

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("111111");
-console.log(111111);
+//1 problem///////////
+
+const arr = [1,9,0,5];
+Array.prototype.last = function() {
+    if(this.length === 0){
+        return -1;
+    }else{
+        return this[this.length - 1];
+    }
+};
+/////////////////////////////
