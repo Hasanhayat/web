3578b2c2ccc55bd54c9a2aa19902c214dd324ab4
hasanhayat <aylskhsmvz.v@gmail.com>
2024-11-26 22:06:29 +0500

    c

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 //1 problem///////////
 
-const arr = [1,9,0,5];
-Array.prototype.last = function() {
-    if(this.length === 0){
-        return -1;
-    }else{
-        return this[this.length - 1];
-    }
+const arr = [1, 9, 0, 5];
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
 };
-/////////////////////////////
+////////////    2620       /////////////////
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+//  console.log(counter());
+//  console.log(counter());
+//  console.log(counter());
+////////////         9    ////////////
+
+var isPalindrome = function(x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+       return false
+       
+   }
+   let reversedHalf = 0;
+   while (x > reversedHalf) {
+       reversedHalf = reversedHalf * 10 + x % 10; 
+       x = Math.floor(x / 10);
+
+   }
+   if(x === reversedHalf || x === Math.floor(reversedHalf / 10)){
+       return true;
+   }
+}; 
+
+
+console.log(isPalindrome(121)); 
+console.log(isPalindrome(-121)); 
+console.log(isPalindrome(11)); 
+
