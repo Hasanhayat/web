3b101d7a9545d8c0eabe2894b213162ed410dd2d
hasanhayat <aylskhsmvz.v@gmail.com>
2024-11-28 20:36:42 +0500

    5

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ Array.prototype.last = function () {
   } else {
     return this[this.length - 1];
   }
-};  
-
+};
 
 ////////////    2620       /////////////////
 var createCounter = function (n) {
@@ -20,37 +19,31 @@ var createCounter = function (n) {
 const counter = createCounter(10);
 //  console.log(counter());
 //  console.log(counter());
-//  console.log(counter());  
-
-
+//  console.log(counter());
 
 ////////////         9    ////////////
 
-var isPalindrome = function(x) {
-    if (x < 0 || (x % 10 === 0 && x !== 0)) {
-       return false
-       
-   }
-   let reversedHalf = 0;
-   while (x > reversedHalf) {
-       reversedHalf = reversedHalf * 10 + x % 10; 
-       x = Math.floor(x / 10);
+var isPalindrome = function (x) {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
+  let reversedHalf = 0;
+  while (x > reversedHalf) {
+    reversedHalf = reversedHalf * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  if (x === reversedHalf || x === Math.floor(reversedHalf / 10)) {
+    return true;
+  }
+};
 
-   }
-   if(x === reversedHalf || x === Math.floor(reversedHalf / 10)){
-       return true;
-   }
-}; 
+// console.log(isPalindrome(121));
+// console.log(isPalindrome(-121));
+// console.log(isPalindrome(11));
 
-// console.log(isPalindrome(121)); 
-// console.log(isPalindrome(-121)); 
-// console.log(isPalindrome(11)); 
-
-////////////////////////////////////////  
-
+////////////////////////////////////////
 
 // console.log("hello, node");
-
 
 ///////2621//////////
 
@@ -59,13 +52,33 @@ var isPalindrome = function(x) {
  * @return {Promise}
  */
 async function sleep(millis) {
-  return new Promise(resolve => setTimeout(()=>{
-    console.log(millis);
-  }, millis));
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log(millis);
+    }, millis)
+  );
 }
 
-sleep(1000)
-/** 
+// sleep(1000)
+/**
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
+
+/////////////////////////////////////
+
+let nums = [3, 2, 4];
+let target = 6;
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log(nums[i], nums[j]);
+        return [i, j];
+      }
+    }
+  }
+
+
+  
+};console.log(twoSum(nums, target)); 
