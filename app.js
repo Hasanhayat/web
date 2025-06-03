04d0a618862bb39cbd426bdb7ea21006edf431eb
Hassan Hayat <aylskhsmvz.v@gmail.com>
2025-06-03 22:25:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ Array.prototype.last = function () {
   } else {
     return this[this.length - 1];
   }
-};
+}
+
+
 
 ////////////    2620       /////////////////
 var createCounter = function (n) {
